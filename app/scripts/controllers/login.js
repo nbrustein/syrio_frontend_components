6b014fe14bb56326067b1060403f8142670b773c
nbrustein <nbrustein@gmail.com>
2013-07-06 21:52:31 -0400

    got a controller from a module into an app

---
+++ app/scripts/controllers/login.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('SyrioFrontendComponents')
+  // .controller('LoginCtrl', ['$scope', function ($scope) {
+  //    console.log('ok');
+  //  }]);
+	.controller('SyrioFrontendComponents.loginCtrl', function(){
+		console.log('ok');
+	});
